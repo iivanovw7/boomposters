0875aae4023845740483fd4b14e6b590243d31eb
iivanovw7 <9iiiuk2@gmail.com>
2018-02-23 22:40:12 +0300

    mobile improvements

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import _ from 'lodash';
 import NavBar from "./NavBar";
 import './MainPage.css';
@@ -11,6 +11,7 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import Whatshot from 'material-ui/svg-icons/social/whatshot';
 import ShoppingCart from 'material-ui/svg-icons/action/shopping-cart';
 import Upload from 'material-ui/svg-icons/action/backup';
+import MobileSidebar from './MobilePanel';
 
 
 const style = {
@@ -56,9 +57,9 @@ const CAT_URL = '../src/img/categories/';
 
 class CategoriesList extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         const ele = document.getElementById('ipl-progress-indicator');
-        if(ele){
+        if (ele) {
             setTimeout(() => {
                 ele.classList.add('available');
                 setTimeout(() => {
@@ -95,90 +96,97 @@ class CategoriesList extends Component {
         return (
 
 
-                <div style={style} className={"mainContainer"}>
-                            <NavBar/>
-                        <div className="row">
-                            <div className="logo">
-                                <a href={"/"}><img className="logo" src={'../src/img/logo.png'}/></a>
-                            </div>
-                            <div className='separator'>
-                                <img className='separator' src={'../src/img/razdelitel.png'}/>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='categories'>
-                                {this.renderCategories()}
-                            </div>
-                        </div>
-                        <div className='separator'>
-                            <img className='separator' src={'../src/img/razdelitel.png'}/>
-                        </div>
-                        <div className="actionIconsContainer flexWrapper">
-                            <div><img src="../../src/icons/actionB/hdmovie.svg" alt="HD качество"/><br></br>
-                                <h2>ОТЛИЧНОЕ КАЧЕСТВО</h2></div>
-                            <div><img src="../../src/icons/actionB/upload.svg" alt="Загрузка изображений"/><br></br>
-                                <h2>ВАШЕ ИЗОБРАЖЕНИЕ</h2></div>
-                            <div><img src="../../src/icons/actionB/delivery.svg" alt="Доставка"/><br></br>
-                                <h2>ДОСТАВКА ПО РФ</h2></div>
-                            <div><img src="../../src/icons/actionB/ok.svg" alt="Легко и просто"/><br></br>
-                                <h2>ЛЕГКО И ПРОСТО</h2></div>
-                        </div>
-                        <div className='flexWrapper' style={ActionBar}>
-                            <div>
-                                <div className={"textContainer"}>
-                                    <h2>Магазин улётных постеров BOOMPOSTERS.RU</h2>
-                                    <ul className={'textContainerLine'}>
-                                        <li>Отличное качество печати</li>
-                                        <li>Плакаты и картины с вашими изображениями</li>
-                                        <li>Быстрое изготовление</li>
-                                        <li>Каталог лучших постеров на стену</li>
-                                        <li>Лучший арт со всей сети</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className={"flexColumn"}>
-                                <RaisedButton
-                                    className={'actionButton'}
-                                    label="ТОП 25 ЛУЧШИХ ПОСТЕРОВ"
-                                    labelPosition="before"
-                                    icon={<Whatshot />}
-                                    style={styles.button}
-                                />
-                                <RaisedButton
-                                    className={'actionButton'}
-                                    label="ЗАКАЗАТЬ НАШИ ПОСТЕРЫ"
-                                    labelPosition="before"
-                                    icon={<ShoppingCart />}
-                                    style={styles.button}
-                                />
-                                <RaisedButton
-                                    className={'actionButton'}
-                                    label="ЗАГРУЗИТЬ ИЗОБРАЖЕНИЕ"
-                                    labelPosition="before"
-                                    icon={<Upload />}
-                                    style={styles.button}
-                                />
-                            </div>
-                        </div>
-                        <div style={footer}>
-                            <div className={'socialPane'}>
-                                <div>
-                                    <IconButton style={iconStyle}>
-                                        <img src="../../src/icons/vk.svg" width="40" height="40" alt="vk"/>
-                                    </IconButton>
-                                    <IconButton style={iconStyle}>
-                                        <img src="../../src/icons/inst.svg" width="38" height="38" alt="instagram"/>
-                                    </IconButton>
-                                </div>
-                            </div>
-                            <div className="flexColumn">
-                                <div className="Copyright">
-                                    <div style={style}>© 2018 BOOMPOSTERS.RU ALL RIGHTS RESERVED</div>
-                                </div>
-                            </div>
-                        </div>
-
+            <div style={style} className={"mainContainer"}>
+                <div className={'mobileNavigation'}>
+                    <MobileSidebar/>
                 </div>
+                <div className={'navigation'}>
+                    <NavBar/>
+                </div>
+                <div className="row">
+                    <div className="logo">
+                        <a href={"/"}><img className="logo" src={'../src/img/logo.png'}/></a>
+                    </div>
+                    <div>
+                        <img className='separator' src={'../src/img/razdelitel.png'}/>
+                        <span className={'separatorMobile'}><h2>BOOMPOSTERS.RU</h2></span>
+                    </div>
+                </div>
+                <div>
+                    <div className='categories'>
+                        {this.renderCategories()}
+                    </div>
+                </div>
+                <div>
+                    <img className='separator' src={'../src/img/razdelitel.png'}/>
+                    <span className={'separatorMobile'}><h2>BOOMPOSTERS.RU</h2></span>
+                </div>
+                <div className="actionIconsContainer flexWrapper">
+                    <div><img src="../../src/icons/actionB/hdmovie.svg" alt="HD качество"/><br></br>
+                        <h2>ОТЛИЧНОЕ КАЧЕСТВО</h2></div>
+                    <div><img src="../../src/icons/actionB/upload.svg" alt="Загрузка изображений"/><br></br>
+                        <h2>ВАШЕ ИЗОБРАЖЕНИЕ</h2></div>
+                    <div><img src="../../src/icons/actionB/delivery.svg" alt="Доставка"/><br></br>
+                        <h2>ДОСТАВКА ПО РФ</h2></div>
+                    <div><img src="../../src/icons/actionB/ok.svg" alt="Легко и просто"/><br></br>
+                        <h2>ЛЕГКО И ПРОСТО</h2></div>
+                </div>
+                <div className='flexWrapper' style={ActionBar}>
+                    <div>
+                        <div className={"textContainer"}>
+                            <h2>Магазин улётных постеров BOOMPOSTERS.RU</h2>
+                            <ul className={'textContainerLine'}>
+                                <li>Отличное качество печати</li>
+                                <li>Плакаты и картины с вашими изображениями</li>
+                                <li>Быстрое изготовление</li>
+                                <li>Каталог лучших постеров на стену</li>
+                                <li>Лучший арт со всей сети</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={"flexColumn"}>
+                        <RaisedButton
+                            className={'actionButton'}
+                            label="ТОП 25 ЛУЧШИХ ПОСТЕРОВ"
+                            labelPosition="before"
+                            icon={<Whatshot/>}
+                            style={styles.button}
+                        />
+                        <RaisedButton
+                            className={'actionButton'}
+                            label="ЗАКАЗАТЬ НАШИ ПОСТЕРЫ"
+                            labelPosition="before"
+                            icon={<ShoppingCart/>}
+                            style={styles.button}
+                        />
+                        <RaisedButton
+                            className={'actionButton'}
+                            label="ЗАГРУЗИТЬ ИЗОБРАЖЕНИЕ"
+                            labelPosition="before"
+                            icon={<Upload/>}
+                            style={styles.button}
+                        />
+                    </div>
+                </div>
+                <div style={footer}>
+                    <div className={'socialPane'}>
+                        <div>
+                            <IconButton style={iconStyle}>
+                                <img src="../../src/icons/vk.svg" width="40" height="40" alt="vk"/>
+                            </IconButton>
+                            <IconButton style={iconStyle}>
+                                <img src="../../src/icons/inst.svg" width="38" height="38" alt="instagram"/>
+                            </IconButton>
+                        </div>
+                    </div>
+                    <div className="flexColumn">
+                        <div className="Copyright">
+                            <div style={style}>© 2018 BOOMPOSTERS.RU ALL RIGHTS RESERVED</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
         );
     }
---
+++ src/components/MobilePanel.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
+import './MobilePanel.css';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import * as Colors from 'material-ui/styles/colors';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import IconButton from 'material-ui/IconButton';
+import ShoppingCart from 'material-ui/svg-icons/action/shopping-cart';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+
+
+const iconStyles = {
+    color: 'white',
+    textColor: 'white',
+    margin: 'auto',
+    align: 'right'
+};
+
+
+const barStyle = {
+    background: '#373a3c',
+};
+
+const muiTheme = getMuiTheme({
+    palette: {
+        background: '#373a3c',
+        textColor: Colors.darkBlack,
+        primary1Color: '#373a3c',
+    },
+    appBar: {
+        background: '#373a3c',
+        height: 50,
+    },
+});
+
+
+export default class MobilePanel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
+    handleToggle = () => this.setState({open: !this.state.open});
+
+
+    render() {
+        return (
+
+            <MuiThemeProvider muiTheme={muiTheme}>
+            <div>
+                <AppBar className={'navBarStyle'}
+                        style={barStyle}
+                        title="Магазин улётных постеров"
+                        onClick={this.handleToggle}>
+                    <FlatButton
+                        style={iconStyles}
+                        icon={<ShoppingCart/>}
+                    />
+                </AppBar>
+
+                <Drawer open={this.state.open}>
+                    <MenuItem primaryText="Комиксы" />
+                    <MenuItem primaryText="Иллюстрации" />
+                    <MenuItem primaryText="Сериалы" />
+                    <MenuItem primaryText="Аниме" />
+                    <MenuItem primaryText="Винтаж" />
+                    <MenuItem primaryText="Ч/Б" />
+                    <MenuItem primaryText="Музыкальные" />
+                    <MenuItem primaryText="Лоу-Арт" />
+                </Drawer>
+
+            </div>
+            </MuiThemeProvider>
+        );
+    }
+}
